33646b630befbfea170e8b3c6e57a5f39eb906bf
Graigor Pierre-Noel <gpnoel111@yahoo.com>
2015-10-15 00:43:06 -0400

    adding my 'repository' for Lab 4
    title

---
+++ README.md
@@ -0,0 +1,9 @@
+README for Graigor Pierre-Noel
+
+The files I submitted were in the form of index.* and I hope that isn't a problem. They are in that naming format because I had to serve the project locally and naming the html as index makes it work.
+
+likes: I looked online for how to format the ajax calls but didn't use any of those sources. Instead I was able to figure out the syntax and see that I needed to call responseData.playlist in the .each loop because how I formatted my json. After that point it was intuitive to be able to call data from my json
+
+dislikes:
+
+obstacles: Chrome wouldn't recognize my flag to turn off web security and allow file access from files so that was a hinderance
---
+++ lab4.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+	//adding an onclick event to the image
+	$("#site").click(function(){
+		//adds the border and styling to the playlist
+		$(".outline").css({"overflow":"hidden", "min-height":"0px", "border-style":"solid", "border-color":"red"});
+
+		//adds <ul> tags to respective containers (divs/headers)
+		$("<ul>").appendTo("#title").css({"width":"150px", "list-style":"none", "float":"left",	"padding-right":"25px",	"padding-left":"0px", "margin":"0px", "width": "auto"});
+   	  	$("<ul>").appendTo("#artist").css({"width":"150px", "list-style":"none", "float":"left",	"padding-right":"25px",	"padding-left":"0px", "margin":"0px", "width": "auto"});
+   	  	$("<ul>").appendTo("#album").css({"width":"150px", "list-style":"none", "float":"left",	"padding-right":"25px",	"padding-left":"0px", "margin":"0px", "width": "auto"});
+   	  	$("<ul>").appendTo("#date").css({"width":"150px", "list-style":"none", "float":"left",	"padding-right":"25px",	"padding-left":"0px", "margin":"0px", "width": "auto"});
+   	  	//since a <ul> tag was already created for me, I just edited its css
+   	  	$("#genres ul").css({"width":"150px", "list-style":"none", "float":"left",	"padding-right":"25px",	"padding-left":"0px", "margin":"0px", "width": "auto"});
+   	  	$("<ul>").appendTo("#artist_link").css({"width":"150px", "list-style":"none", "float":"left",	"padding-right":"25px",	"padding-left":"0px", "margin":"0px", "width": "auto"});
+   	  	$("<ul>").appendTo("#cover").css({"width":"150px", "list-style":"none", "float":"left",	"padding-right":"25px",	"padding-left":"0px", "margin":"0px", "width": "auto"});
+
+		$.ajax({
+			//gets the json file and checks if that process went ok
+	   	 	type: "GET",
+	   	 	url: "lab4.json",
+	   	 	dataType: "json",
+	   	 	success: function(responseData, status){
+		   	  	//go through each element in my 'playlist' array and output information to respective spots in the html
+	    	 	$.each(responseData.playlist, function(i, item) {
+	    	 		//using jquery selectors for styling slightly different to each <li> element added
+
+		        	$("<li>"+item.track_name+"</li>").appendTo("#title ul").css({"height":"150px", "padding":"10px", "font-variant":"small-caps", "color":"green", "font-size":"85%"});
+		        	$("<li>"+item.artist+"</li>").appendTo("#artist ul").css({"height":"150px",	"padding":"10px", "font-weight":"bold"});
+		        	$("<li>"+item.album+"</li>").appendTo("#album ul").css({"height":"150px", "padding":"10px", "font-style":"italic"});
+		        	$("<li>"+item.date+"</li>").appendTo("#date ul").css({"height":"150px",	"padding":"10px", "font-size":"75%"});
+		        	$("<li>"+item.genres+"</li>").appendTo("#genres ul").css({"height":"150px", "padding":"10px", "font-size":"75%", "list-style-type":"circle"});
+		        	$("<li><a href="+item.artist_site_url+">"+item.artist+" link</a></li>").appendTo("#artist_link ul").css({"height":"150px", "padding":"10px"});
+		        	$("<li><img src="+item.album_art_url+" alt="+item.album+" style=width:150px;height:150px;></li>").appendTo("#cover ul");
+	      		});
+	    	}, error: function(msg) {
+	      		//output for if an error occurred
+	      		alert("There was a problem: " + msg.status + " " + msg.statusText);
+	    	}
+	  	});
+	});
+});
